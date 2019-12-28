c742353b4cd4a76350c161309c1f1f843247802c
ikx <ihsankx@gmail.com>
2019-12-28 14:30:07 +0000

    added class

--- programming_challenges/O7/O7_number_game/main.py
+++ programming_challenges/O7/O7_number_game/main.py
@@ -1,29 +1,26 @@
 import random
 
 
-class NumberGame:
-    def __init__(self):
-        self.guess = -1
-        self.guesses = 0
-        self.answer = random.randint(1, 100)
+def play_number_game(self):
+    guess = -1
+    guesses = 0
+    answer = random.randint(1,100)
 
-    def play_number_game(self):
-        while self.guess != self.answer:
-            self.guesses += 1
-            try:
-                self.guess = int(input("Guess a number between 1 and 100: "))
-                if self.guess == self.answer:
-                    print("You got the answer in " + str(self.guesses) + " guesses!")
-                elif self.guess >= self.answer:
-                    print("Too high!")
-                    print("You have guessed " + str(self.guesses) + " times.")
-                elif self.guess <= self.answer:
-                    print("Too low!")
-                    print("You have guessed " + str(self.guesses) + " times.")
-            except Exception:
-                print("That is not an integer.")
-                print("You have guessed " + str(self.guesses) + " times.")
+    while guess != answer:
+        guesses += 1
+        try:
+            guess = int(input("Guess a number between 1 and 100: "))
+            if guess == answer:
+                print("You got the answer in " + str(guesses) + " guesses!")
+            elif guess >= answer:
+                print("Too high!")
+                print("You have guessed " + str(guesses) + " times.")
+            elif guess <= answer:
+                print("Too low!")
+                print("You have guessed " + str(guesses) + " times.")
+        except Exception:
+            print("That is not an integer.")
+            print("You have guessed " + str(guesses) + " times.")
 
 
-number_game = NumberGame()
-number_game.play_number_game()
+play_number_game()
